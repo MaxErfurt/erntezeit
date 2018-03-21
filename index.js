b1566c882167a9878055826edbab699f25f1cfba
MaxErfurt <maximum.effort@posteo.de>
2018-03-21 17:13:29 +0100

    Update index.js
    Else (wrong, Its python...) entfernt.

--- index.js
+++ index.js
@@ -13,15 +13,6 @@ var handlers = {
     if (myProdukt == "apfel") {
         this.response.speak("Die meisten Apfelsorten werden im Oktober und September geerntet. Aber viele Sorten gibt es das ganze Jahr lang aus Lagerung. Gut Biss!");
     }
-    or {
-      'ProductIntent': function () {
-        var myProdukt = this.event.request.intent.slots.produkt.value;
-        if (myProdukt == "apfel") {
-        this.response.speak("Die meisten Apfelsorten werden im Oktober und September geerntet. Aber viele Sorten gibt es das ganze Jahr lang aus Lagerung. Gut Biss!");
-    } 
-    else {
-        this.response.speak("You guessed that " + myProdukt + " is the most popular. Actually, Python is our most popular language");
-    }
     this.emit(':responseReady');
   }
 };
