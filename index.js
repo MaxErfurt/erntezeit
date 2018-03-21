e0787945783f706475f00d95fb2050ebfefa9297
MaxErfurt <maximum.effort@posteo.de>
2018-03-21 16:30:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var Alexa = require("alexa-sdk");
 
 var handlers = {
   'LaunchRequest': function() {
-    this.response.speak("Von welchem Obst oder Gemüse möchtest du wissen, ob es Saison hat?").listen("Ich hab dich nicht verstanden. Von welchem Obst oder Gemüse möchtest du wissen, ob es Saison hat?");
+    this.response.speak("Von welchem Obst oder Gemüse möchtest du wissen, ob es Saison hat?").listen("Ich habe dich nicht verstanden. Von welchem Obst oder Gemüse möchtest du wissen, ob es Saison hat?");
     this.emit(':responseReady');
   },
 
@@ -17,6 +17,7 @@ var handlers = {
         this.response.speak("You guessed that " + myProdukt + " is the most popular. Actually, Python is our most popular language");
     }
     this.emit(':responseReady');
+  }
 }
 
 exports.handler = function(event, context, callback){
